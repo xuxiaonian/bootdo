880198da4c0b87323b5fa9f8216caeb5db0a49bd
xuxiaonian <2496875679@qq.com>
2018-03-14 18:37:35 +0800

    上线准备1

--- src/main/java/com/bootdo/BootdoApplication.java
+++ src/main/java/com/bootdo/BootdoApplication.java
@@ -14,51 +14,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//@EnableTransactionManagement
-////@MapperScan("com.bootdo.*.dao")
-//@SpringBootApplication
-//@EnableScheduling
-//public class BootdoApplication {
-//	public static void main(String[] args) {
-//		SpringApplication.run(BootdoApplication.class, args);
-//		System.out.println("*****************************");
-//		System.out.println("*****************************");
-//		System.out.println("*******BootDo启动成功**********");
-//		System.out.println("*****************************");
-//		System.out.println("*****************************");
-//	}
-//
-//
-//	@Bean
-//	public EmbeddedServletContainerCustomizer containerCustomizer() {
-//		return (container -> {
-//			ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
-//			ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
-//			ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
-//			container.addErrorPages(error401Page, error404Page, error500Page);
-//			container.setSessionTimeout(1800);// 单位为S
-//		});
-//
-//	}
-//}
-
-
 @EnableTransactionManagement
+//@MapperScan("com.bootdo.*.dao")
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @EnableScheduling
-@MapperScan("com.bootdo.system.dao.MenuMapper")
-public class BootdoApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(BootdoApplication.class);
-	}
-
-
+public class BootdoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootdoApplication.class, args);
+		System.out.println("*****************************");
+		System.out.println("*****************************");
+		System.out.println("*******BootDo启动成功**********");
+		System.out.println("*****************************");
+		System.out.println("*****************************");
 	}
+
+
 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer() {
 		return (container -> {
